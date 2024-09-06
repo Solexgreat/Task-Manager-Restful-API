7ea7e1ccd84e5040a83cf2e8814e61986db9aa36
Solexgreat <solomonGodkid@gmail.com>
2024-09-06 03:32:22 +0100

    Task-Manager-Project

--- backend/column/app/v1/users/models.py
+++ backend/column/app/v1/users/models.py
@@ -1,11 +1,18 @@
-from mongoengine import Document, StringField, DateTimeField, ReferenceField
+from mongoengine import Document, StringField, DateTimeField, BooleanField, connect
 import datetime
+from ...app import db
+
+connect(db='column', alias='default', host='mongodb+srv://Solexgreat:solexgreat1$@cluster0.wekq3.mongodb.net')
 
 
 # Insert a new user
 class User(Document):
-    username = StringField(requried=True, unique=True)
-    email = StringField(requried=True, unique=True)
+    username = StringField(max_length=200, requried=True, unique=True)
+    email = StringField(max_length=100, requried=True, unique=True)
+    last_name = StringField(max_length= 200)
+    first_name = StringField(max_length= 200)
+    is_active = BooleanField(required=True, default=True)
+    is_superuser = BooleanField(required=True, default=True)
     password_hash = StringField(requried=True)
-    created_at = DateTimeField(default=datetime.datetime.utcnow())
-    updated_at = DateTimeField(default=lambda: datetime.datetime.now(datetime.timezone.utcnow()))
+    created_at = DateTimeField(default=datetime.datetime.timestamp())
+    updated_at = DateTimeField(default=datetime.datetime.timestamp())
