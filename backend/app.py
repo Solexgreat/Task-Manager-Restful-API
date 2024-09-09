07d5491061ed3b5febaa57af3df15e22b62e0eec
Solexgreat <solomonGodkid@gmail.com>
2024-09-09 17:16:34 +0100

    Task-Manager-Project

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, request, jsonify
-from pymongo import MongoClient
-from bson.json_util import dumps
-from column.app.v1.users.models import User
-from column.app.v1.users.schemas import UserCreate
-from column.app.v1.users.controller import create_user, get_user_by_Id, get_user_by_email, update_user_by_id
-from column.app.v1.core.security import get_reset_password_token, update_password
-from werkzeug.exceptions import BadRequest, InternalServerError
-from bson import ObjectId, json_util
+
 from column.app.v1.custom_base_schemas import PyObjectId
 import json
 import datetime
 
 
+from routes import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 app = Flask(__name__)
-client = MongoClient("mongodb+srv://Solexgreat:solexgreat1$@cluster0.wekq3.mongodb.net/")
-db = client.flask_example_db
+
 
 
 
--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, request, jsonify
+from pymongo import MongoClient
 from column.app.v1.users.controller import create_user, get_user_by_Id, get_user_by_email, update_user_by_id
 from column.app.v1.core.security import get_reset_password_token, update_password
 from werkzeug.exceptions import BadRequest
 from column.app.v1.users.models import User
 from column.app.v1.users.schemas import UserCreate
+from bson import ObjectId, json_util
+from bson.json_util import dumps
 from column.app.v1.custom_base_schemas import PyObjectId
 
 user_bp = Blueprint('user', __name__)
 
-
+client = MongoClient("mongodb+srv://Solexgreat:solexgreat1$@cluster0.wekq3.mongodb.net/")
+db = client.flask_example_db
 
 
 @user_bp.route('/')
